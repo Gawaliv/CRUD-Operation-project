2aedf312df0aa378e75450b6ba3a2b398a62d71b
Gawaliv <gawalivaishnavi39@gmail.com>
2025-02-11 19:22:26 +0530

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,15 @@
-namespace CrudOperationProject
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectCRUD
 {
     public partial class Form1 : Form
     {
@@ -7,21 +18,47 @@ namespace CrudOperationProject
             InitializeComponent();
         }
 
-       
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Databasecrud;Integrated Security=True;Pooling=False;Encrypt=True;Trust Server Certificate=True");
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-28EVAV8\\MSSQLSERVER01;Initial Catalog=ProjectCRUDoperation;Integrated Security=True;Encrypt=False");
             con.Open();
-            sqlCommand cmd = new sqlCommand("insert into UserTab values (@id,@Name,@Age)",con);
-            cmd.parameter.AddWithValue("@ID",int.Parse(textBox1.Text));
-            cmd.parameter.AddWithValue("@Name",textBox2.Text);
-            cmd.parameter.AddWithValue("@Age", double.Parse(textBox3.Text));
+            SqlCommand cmd = new SqlCommand("insert into Cruddata values(@ID,@Name,@Age)",con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name",textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
             cmd.ExecuteNonQuery();
-
             con.Close();
-            MessageBox.Show("Successfully Saved");
+
+            MessageBox.Show("Successfully saved");
         }
 
- 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-28EVAV8\\MSSQLSERVER01;Initial Catalog=ProjectCRUDoperation;Integrated Security=True;Encrypt=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update cruddata set Name=@Name, Age=@Age where ID=@ID",con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Update");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-28EVAV8\\MSSQLSERVER01;Initial Catalog=ProjectCRUDoperation;Integrated Security=True;Encrypt=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete cruddata where ID=@ID",con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully deleted");
+
+
+        }
     }
 }
